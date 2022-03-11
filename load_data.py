4d1a425c977e3b85f243978e0ea02fa39044cbf1
katie-lim <35044591+katie-lim@users.noreply.github.com>
2022-03-11 09:49:07 +0000

    reorganise functions

--- load_data.py
+++ load_data.py
@@ -1,4 +1,7 @@
+import numpy as np
 from astropy.io import fits
+from astropy.visualization import ImageNormalize, ZScaleInterval
+
 
 filePath = "A1_mosaic.fits"
 
@@ -13,3 +16,43 @@ def loadHeader(filePath):
 
 originalImage = loadImage(filePath)
 header = loadHeader(filePath)
+
+
+
+def getImageWithMask(image, threshold):
+    return np.ma.masked_array(image, image < threshold, fill_value=0)
+
+
+
+def maskVerticalLine(imageWithMask, xmin, xmax, ylower, yupper):
+    image = imageWithMask.data
+    height, width = image.shape
+
+    for y in range(0, height):
+        if (ylower <= y <= yupper):
+            continue
+
+        for x in range (xmin, xmax):
+            imageWithMask[y, x] = np.ma.masked
+
+    return imageWithMask
+
+
+
+def cropImage(image, xmin=0, xmax=None, ymin=0, ymax=None):
+    return image[ymin:ymax, xmin:xmax]
+
+
+
+def plotZScale(image):
+    height, width = image.shape
+
+    norm = ImageNormalize(image, ZScaleInterval())
+
+    fig = plt.figure(figsize=(3, 8), dpi=400)
+    ax = fig.add_subplot(1, 1, 1)
+    im = ax.imshow(image, norm=norm, origin="lower", cmap="gray")
+    plt.xlim(0, width)
+    plt.ylim(0, height)
+
+    fig.colorbar(im)
--- source_detection.py
+++ source_detection.py
@@ -1,51 +1,10 @@
 # %%
-import numpy as np
 from numba import jit
+import numpy as np
 import matplotlib.pyplot as plt
 from uncertainties import unumpy
-from astropy.visualization import ImageNormalize, ZScaleInterval
 from matplotlib.patches import Circle
-
-
-
-def plotZScale(image):
-    height, width = image.shape
-
-    norm = ImageNormalize(image, ZScaleInterval())
-
-    fig = plt.figure(figsize=(3, 8), dpi=400)
-    ax = fig.add_subplot(1, 1, 1)
-    im = ax.imshow(image, norm=norm, origin="lower", cmap="gray")
-    plt.xlim(0, width)
-    plt.ylim(0, height)
-
-    fig.colorbar(im)
-
-
-
-def getImageWithMask(image, threshold):
-    return np.ma.masked_array(image, image < threshold, fill_value=0)
-
-
-
-def maskVerticalLine(imageWithMask, xmin, xmax, ylower, yupper):
-    image = imageWithMask.data
-    height, width = image.shape
-
-    for y in range(0, height):
-        if (ylower <= y <= yupper):
-            continue
-
-        for x in range (xmin, xmax):
-            imageWithMask[y, x] = np.ma.masked
-
-    return imageWithMask
-
-
-
-
-def cropImage(image, xmin=0, xmax=None, ymin=0, ymax=None):
-    return image[ymin:ymax, xmin:xmax]
+from load_data import *
 
 
 
